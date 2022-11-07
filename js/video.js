30c0940631fbde9852455be8a3761075c80736e2
zghandou <112430345+zghandou@users.noreply.github.com>
2022-11-07 10:35:07 -0500

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,28 @@
+var video = document.querySelector("#player1");
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+	video.autoplay = false;
+	video.loop = false;
+
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+ 	console.log("Play Video");
+	document.querySelector("#volume").innerHTML = video.volume *100 + "%"
+	video.play()
+	
+ });
+ document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause()
+
+});
+document.querySelector("#slower").addEventListener("click", function() {
+	console.log("Slow Down Video");
+	video.playbackRate = 0.5;
+});
+document.querySelector("#faster").addEventListener("click", function() {
+	console.log("Speed up Video");
+	video.playbackRate = 1.0;
+});
